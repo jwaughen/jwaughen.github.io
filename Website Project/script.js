6b8eb85ad57701b3b7041a702a2adc775f5c067d
jwaughen <jlwaughen@gmail.com>
2022-10-31 18:11:59 -0400

    Added Website Project

---
+++ Website Project/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    //Adding logo to all HTML pages
+    $('.logo').html("<a href=\"index.html\"><img src=\"images/Sweet Escapes Logo.png\"></a>");
+
+    //Adding footer to all HTML pages
+    $('footer').html("<p>In Progress</p></br>");
+
+});
+
